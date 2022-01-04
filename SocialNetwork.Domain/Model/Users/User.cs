6f649747f886c4825e15872e72d49d81c9dbacc6
Jesus Herruzo <herruzojm@gmail.com>
2022-01-04 10:44:03 +0100

    Add user model

---
+++ SocialNetwork.Domain/Model/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Model.Users
+{
+    public class User
+    {
+        public string UserName { get; }
+
+        public User(string userName)
+        {
+            UserName = userName;
+        }
+
+    }
+}
---
+++ SocialNetwork.Domain/Model/Users/UserValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Model.Users
+{
+    public class UserValidator
+    {
+        public bool IsValid(User user)
+        {
+            return IsValid(user.UserName);
+        }
+
+        public bool IsValid(string userName)
+        {
+            // no empty names
+            // no spaces in the name
+            if (userName.Length == 0)
+            {
+                return false;
+            }
+
+            if (userName.Any(Char.IsWhiteSpace))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
