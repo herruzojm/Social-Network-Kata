5592b0ec454979cff63ff68de07ff9ea31a145aa
Jesus Herruzo <herruzojm@gmail.com>
2022-01-04 19:19:48 +0100

    Add messages modl

---
+++ SocialNetwork.Domain/Model/Messages/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Domain.Model.Messages
+{
+    public class Message
+    {
+        public string UserName { get; }
+
+        public DateTime CreationTime { get; }
+
+        public string Text { get; }
+
+        public Message(string userName, string text)
+        {
+            UserName = userName;
+            Text = text;
+            CreationTime = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+}
